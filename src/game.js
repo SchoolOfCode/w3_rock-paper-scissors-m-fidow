9e99b454e17c45a43103c7bcc33d2a0058ac4b13
m-fidow <muna.a.fidow@gmail.com>
2021-07-18 17:59:49 +0100

    imported images

--- src/game.js
+++ src/game.js
@@ -1,47 +1,50 @@
 import React from "react";
+import Rock from "./images/rock.svg";
+import Paper from "./images/paper.svg";
+import Scissors from "./images/scissors.svg";
 
 const Game = () => {
   return (
     <main>
       <h1>
-        Hello, <span class="player-name">Player</span>
+        Hello, <span className="player-name">Player</span>
       </h1>
-      <section class="scoreboard">
-        <div class="games">
+      <section className="scoreboard">
+        <div className="games">
           <h2>
             Games <span>0</span>
           </h2>
         </div>
-        <div class="wins">
+        <div className="wins">
           <h2>Wins</h2>
-          <span class="span">0</span>
+          <span className="span">0</span>
         </div>
-        <div class="losses">
+        <div className="losses">
           <h2>Losses</h2>
-          <span class="span">0</span>
+          <span className="span">0</span>
         </div>
-        <div class="draws">
+        <div className="draws">
           <h2>Draws</h2>
-          <span class="span">0</span>
+          <span className="span">0</span>
         </div>
       </section>
-      <section class="icons">
+      <section className="icons">
         <div id="rock">
-          <img src="/images/rock.svg" alt="" />
+          <img src={Rock} alt="" />
         </div>
         <div id="paper">
-          <img src="/images/paper.svg" alt="" />
+          <img src={Paper} alt="" />
         </div>
         <div id="scissors">
-          <img src="/images/scissors.svg" alt="" />
+          <img src={Scissors} alt="" />
         </div>
       </section>
-      <section class="button">
-        <button class="reset-button">Reset</button>
+      <section className="button">
+        <button className="reset-button">Reset</button>
       </section>
-      <section class="input">
-        <input type="text" id="#input" maxlength="10" />
-        <button class="submit-button">Submit</button>
+      <section className="input">
+        <input type="text" id="#input" maxLength="10" />
+        <button className="submit-button">Submit</button>
       </section>
     </main>
   );
